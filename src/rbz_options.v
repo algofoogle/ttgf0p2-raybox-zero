766723d508aeb8d19c1e2ee514c146f1ce7ce642
Anton Maurovic <anton@foogle.com>
2025-11-19 19:34:57 +1030

    Drop fixed-point maths to Q10.10

--- src/rbz_options.v
+++ src/rbz_options.v
@@ -16,16 +16,16 @@
 //`define NO_DIV_WALLS
 // Use alternate fixed-point config that goes for the minimum (currently) supported by RBZ:
 `define ALT_FIXED_POINT_PARAMS
-    `define Qm    12   // Signed. 9 is minimum: Below 9, texv is broken. Below 8, rayAddend overflows.
-    `define Qn    12   // Currently 9 is lowest possible because of other bit-range maths, but 10+ is recommended.
-    `define Qmnc  24   // <== MUST EQUAL Qmn+Qn. Sort of the same as `Qmn, but that isn't useful for all my Verilog needs.
+    `define Qm    10   // Signed. 9 is minimum: Below 9, texv is broken. Below 8, rayAddend overflows.
+    `define Qn    10   // Currently 9 is lowest possible because of other bit-range maths, but 10+ is recommended.
+    `define Qmnc  20   // <== MUST EQUAL Qmn+Qn. Sort of the same as `Qmn, but that isn't useful for all my Verilog needs.
     // The following matches LZC config (in lzc.v) to the above...
     //NOTE: The FIRST defined of the following will be used:
     // `define D17 // 17-bit range, e.g. Q8.9
     // `define D18 // 18-bit range, e.g. Q9.9
     // `define D19 // 19-bit range, e.g. Q9.10
-    // `define D20 // 20-bit range, e.g. Q10.10
+    `define D20 // 20-bit range, e.g. Q10.10
     // `define D22 // 22-bit range, e.g. Q11.11
-    `define D24 // 24-bit range, e.g. Q12.12
+    // `define D24 // 24-bit range, e.g. Q12.12
     // `define D30 // 30-bit range, e.g. Q15.15, mostly for testing.
 
